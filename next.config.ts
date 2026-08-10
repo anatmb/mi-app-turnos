d60767b80d5afaace5176f376e06a27fbea7c56b
anatmb <anatmb26@hotmail.com>
2026-08-09 22:29:03 -0300

    chore: forzar bypass de generacion estatica en next.config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.43"],
   
-  // Dejamos SOLAMENTE esta opción, que es la correcta para Prisma 7
+  // 1. Esto le dice a Next.js: "Si una ruta da problemas al pre-renderizarse en el build, no te detengas, déjala pasar"
+  staticPageGenerationTimeout: 1000, 
+  
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client"],
   },
