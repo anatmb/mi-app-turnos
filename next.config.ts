49b611a4e74f299b9d90cf0d2db8e9d5ab58cbf5
anatmb <anatmb26@hotmail.com>
2026-08-09 22:18:24 -0300

    fix: eliminar conflicto de transpilePackages para prisma

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.43"],
   
-  // Esto obliga al compilador a tratar a Prisma de manera segura en el entorno aislado de Vercel
-  transpilePackages: ["@prisma/client"],
+  // Dejamos SOLAMENTE esta opción, que es la correcta para Prisma 7
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client"],
   },
